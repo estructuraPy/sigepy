95ce0b08bccf1a497d37bef11e63033c6cc5d865
ingahnavarro <102553393+ingahnavarro@users.noreply.github.com>
2025-04-13 00:42:31 -0600

    updated

---
+++ conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
+# -- Project information -----------------------------------------------------
+
+project = 'SigePy'
+copyright = '2025, Angel Navarro-Mora'
+author = 'Angel Navarro-Mora'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,14 @@
+Modules
+=======
+
+.. automodule:: sigepy.fourier
+    :members:
+
+.. automodule:: sigepy.ssi_cov
+    :members:
+
+.. automodule:: sigepy.wavelet
+    :members:
+
+.. automodule:: sigepy.utils
+    :members:
